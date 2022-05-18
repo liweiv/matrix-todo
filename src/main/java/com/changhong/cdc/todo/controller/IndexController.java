09d4595b5d3671c59f6409ff5052bf4b71f9fec3
veily <drmfly.liw@gmail.com>
2022-05-18 17:35:57 +0800

    init

---
+++ src/main/java/com/changhong/cdc/todo/controller/IndexController.java
@@ -0,0 +1,13 @@
+package com.changhong.cdc.todo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+    @GetMapping("index")
+    public String index(Model model) {
+        return "index";
+    }
+}
