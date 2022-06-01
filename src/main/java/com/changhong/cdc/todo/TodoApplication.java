b1d279c67c104d0a8a8e4e12e6f7ab362d4c788f
liwei <wei9.li@changhong.com>
2022-06-01 15:48:17 +0800

    add apollo support

--- src/main/java/com/changhong/cdc/todo/TodoApplication.java
+++ src/main/java/com/changhong/cdc/todo/TodoApplication.java
@@ -3,13 +3,25 @@ package com.changhong.cdc.todo;
 import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @EnableApolloConfig
+@EnableDiscoveryClient(autoRegister = true)
 @SpringBootApplication
+@EnableFeignClients
 public class TodoApplication {
+    @LoadBalanced
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(TodoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TodoApplication.class, args);
+    }
 
 }
--- src/main/java/com/changhong/cdc/todo/controller/ApiController.java
+++ src/main/java/com/changhong/cdc/todo/controller/ApiController.java
@@ -2,16 +2,28 @@ package com.changhong.cdc.todo.controller;
 
 import com.changhong.cdc.todo.config.CustomConfig;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @Slf4j
 @RestController
 @RequestMapping("/api/v1/todos")
 public class ApiController {
 
+    @Autowired
+    RestTemplate restTemplate;
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    @Autowired
+    private LoadBalancerClient loadBalancerClient;
     final CustomConfig config;
 
     public ApiController(CustomConfig config) {
@@ -24,5 +36,18 @@ public class ApiController {
 
         String[] todos = {"First", "Second", "Third"};
         return ResponseEntity.ok(todos);
+    }
+
+    @GetMapping("/test")
+    public String test() {
+        ServiceInstance serviceInstance =    loadBalancerClient.choose("helloworld.http");
+//        String url = "http://" + serviceInstance.getServiceId() + ":" + serviceInstance.getPort() + "/member/info/getUserMember/" + username;
+        return restTemplate.getForObject("http://helloworld.http/helloworld/cde", String.class);
+    }
+
+
+    @GetMapping("/services")
+    public Object services() {
+        return discoveryClient.getServices();
     }
 }
